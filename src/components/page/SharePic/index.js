7b1ddbee6f234e074947a943541e2b75597968b4
tangyu <15292061516@163.com>
2019-04-10 23:49:43 +0800

    完成了作品发布页面的主要数据state,还差图片路径

--- src/components/page/SharePic/index.js
+++ src/components/page/SharePic/index.js
@@ -1,46 +1,104 @@
 import React, { Component } from "react";
-import Top from '../../common/top';
+import Top from "../../common/top";
 import ImagePicker from "./ImagePicker";
 
-import './index.scss';
+import "./index.scss";
 
 export default class SharePic extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      work: {
+        w_id: Date.now(), //作品id
+        w_title: "", //作品标题
+        w_content: "", //作品描述
+        w_sort: "",//作品类别},
+      },
+      sorts:[]
+    };
   }
-  Click(){
-   
+  titleChange(e) {
+    let val = e.target.value;
+    this.setState((state)=>{
+      state.work.w_title = val;
+      return state;
+    });
+    console.log(this.state.work);
+  }
+  contentChange(e) {
+    let val = e.target.value;
+    this.setState((state) => {
+      state.work.w_content =val;
+      return state;
+    });
+    console.log(this.state.work);
+  }
+  getSort(e) {
+    let val = e.target.innerHTML;
+    this.setState(state => {
+      state.work.w_sort = val;
+      return state;
+    });
+    console.log(e.target.innerHTML);
+  }
+  componentDidMount() {
+    this.$axios.get("http://localhost:3001/admin/getType").then(res => {
+      let result = res.data;
+      this.setState({sorts:result});
+      console.log(this.state.sorts);
+    });
   }
   render() {
     return (
       <div className="sharePic">
-        <Top />
+        <Top work={this.state.work}/>
         <section>
           <ul className="photo-domain">
             <li>
-              <ImagePicker></ImagePicker>
+              <ImagePicker />
             </li>
           </ul>
           <ul className="title-brief">
             <li>
               <span>作品标题</span>
-              <input type="text" placeholder="请输入" />
+              <input
+                type="text"
+                onChange={e => {
+                  this.titleChange(e);
+                }}
+                placeholder="请输入"
+              />
             </li>
             <li>
               <span>作品简介</span>
-              <textarea placeholder="请输入" />
+              <textarea
+                onChange={e => {
+                  this.contentChange(e);
+                }}
+                placeholder="请输入"
+              />
             </li>
           </ul>
           <dl className="classify">
             <dt>
               <span>选择分类</span>
-              <input type="text"/>
             </dt>
             <dd>
               <ul>
-                <li>动画</li>
+               {
+                 this.state.sorts.map((ele,ind)=>{
+                   return (
+                     <li key={ind}
+                       onClick={e => {
+                         this.getSort(e);
+                       }}
+                     >
+                       {ele.s_type}
+                </li>
+                   )
+                 })
+               }
               </ul>
             </dd>
           </dl>
