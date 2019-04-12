283a55e94e23d4927586dd4c6e6a0377c83942f5
tangyu <15292061516@163.com>
2019-04-12 11:22:38 +0800

    作品发布基本完成，还剩下点击发布，将数据存储到数据库中

--- src/components/page/SharePic/ImagePicker/index.js
+++ src/components/page/SharePic/ImagePicker/index.js
@@ -1,24 +1,11 @@
-import { ImagePicker, WingBlank } from 'antd-mobile';
-import React from 'react'
-const data = [];
+import { ImagePicker, WingBlank } from "antd-mobile";
+import React from "react";
+// const data = [];
 
 class ImagePickerExample extends React.Component {
   state = {
-    files: data,
-    multiple: 1
-  };
-  onChange = (files,type,index) => {
-    this.setState({
-      files
-    });
-      var formData = new FormData();
-      if (files.length > 0) {
-          for (let i = 0; i < files.length; i++) {
-              formData.append('files', files[i].file);
-          }
-      }
-      console.log(formData.getAll("files"));
-      this.postData(formData);
+    files: this.props.files,
+    multiple: 1 //上传多张图片
   };
   onSegChange = e => {
     const index = e.nativeEvent.selectedSegmentIndex;
@@ -26,27 +13,17 @@ class ImagePickerExample extends React.Component {
       multiple: index === 1
     });
   };
-  postData(formData){
-      this.$axios({
-        method: "post",
-        url: "http://localhost:3001/admin/upload",
-        data: formData,
-        headers: { "Content-Type": "multipart/form-data" }
-      }).then(res => {
-        console.log(res);
-      });
-  }
   render() {
     const { files } = this.state;
     return (
       <WingBlank>
         <ImagePicker
-                    files={files}
-                    onChange={this.onChange}
-                    onImageClick={(index, fs) => console.log(index, fs)}
-                    selectable={files.length < 7}
-                    multiple={this.state.multiple}
-                />
+          files={this.props.files}
+          onChange={this.props.photoChange}
+          onImageClick={(index, fs) => console.log(index, fs)}
+          selectable={files.length < 7}
+          multiple={this.state.multiple}
+        />
       </WingBlank>
     );
   }
--- src/components/page/SharePic/index.js
+++ src/components/page/SharePic/index.js
@@ -7,56 +7,94 @@ import "./index.scss";
 export default class SharePic extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       work: {
         w_id: Date.now(), //作品id
         w_title: "", //作品标题
         w_content: "", //作品描述
-        w_sort: "",//作品类别},
+        w_sort: "", //作品类别},
+        photos: []
       },
-      sorts:[]
+      files:[],
+      sorts: []
     };
   }
+  //改变作品图片
+  photoChange(files,type,index) {
+    this.setState({files:files});
+    var formData = new FormData();
+    if (files.length > 0) {
+      for (let i = 0; i < files.length; i++) {
+        formData.append("files", files[i].file);
+      }
+    }
+  }
+  //发布作品
+  releaseWork(){
+
+  }
+  //保存图片到服务器并返回图片路径
+  postData(formData) {
+    this.$axios({
+      method: "post",
+      url: "http://localhost:3001/admin/upload",
+      data: formData,
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    }).then(res => {
+      let data=res.data;
+      //status==1表示成功获取到数据
+     if(res.status===1){
+       console.log(data.path);
+     };
+    });
+  }
+  //改变作品标题
   titleChange(e) {
     let val = e.target.value;
-    this.setState((state)=>{
+    this.setState(state => {
       state.work.w_title = val;
       return state;
     });
     console.log(this.state.work);
   }
+  //改变作品内容
   contentChange(e) {
     let val = e.target.value;
-    this.setState((state) => {
-      state.work.w_content =val;
+    this.setState(state => {
+      state.work.w_content = val;
       return state;
     });
     console.log(this.state.work);
   }
-  getSort(e) {
-    let val = e.target.innerHTML;
+  //获取作品类型
+  getSort(ele) {
+    console.log(ele.s_id);
     this.setState(state => {
-      state.work.w_sort = val;
+      state.work.w_sort = ele.s_id;
       return state;
     });
-    console.log(e.target.innerHTML);
   }
+  //生命周期钩子函数
   componentDidMount() {
     this.$axios.get("http://localhost:3001/admin/getType").then(res => {
       let result = res.data;
-      this.setState({sorts:result});
+      this.setState({ sorts: result });
       console.log(this.state.sorts);
     });
   }
   render() {
     return (
       <div className="sharePic">
-        <Top work={this.state.work}/>
+        <Top/>
         <section>
           <ul className="photo-domain">
             <li>
-              <ImagePicker />
+              <ImagePicker
+                files={this.state.files}
+                photoChange={this.photoChange.bind(this)}
+              />
             </li>
           </ul>
           <ul className="title-brief">
@@ -86,19 +124,18 @@ export default class SharePic extends Component {
             </dt>
             <dd>
               <ul>
-               {
-                 this.state.sorts.map((ele,ind)=>{
-                   return (
-                     <li key={ind}
-                       onClick={e => {
-                         this.getSort(e);
-                       }}
-                     >
-                       {ele.s_type}
-                </li>
-                   )
-                 })
-               }
+                {this.state.sorts.map(ele => {
+                  return (
+                    <li
+                      key={ele.s_id}
+                      onTouchEnd={() => {
+                        this.getSort(ele);
+                      }}
+                    >
+                      {ele.s_type}
+                    </li>
+                  );
+                })}
               </ul>
             </dd>
           </dl>
